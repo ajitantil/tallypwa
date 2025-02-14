fa05a86bd1e495c3dab1684b47012449177775e5
Ajit Antil <ajit.antil@tallysolutions.com>
2025-02-14 15:23:28 +0530

    added manifest in index

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_A1 = "PWA-cache-2";
 
-const filetocache = ["/server.js", "/index.html", "/manifest.json", "/sw.js"];
+const filetocache = ["/index.html", "/manifest.json", "/sw.js"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
