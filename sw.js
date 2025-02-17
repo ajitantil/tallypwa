c541bd83f1ad315955d0350e6c5511c75c95d8a2
Ajit Antil <ajit.antil@tallysolutions.com>
2025-02-17 18:42:09 +0530

    delete old cache

--- sw.js
+++ sw.js
@@ -1,11 +1,26 @@
-const CACHE_A1 = "PWA-cache-2";
+const CACHE_A1 = "PWA-cahe-5";
 
-const filetocache = ["/index.html", "/manifest.json", "/sw.js"];
+const filetocache = ["/manifest.json", "/sw.js"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_A1).then((cache) => {
       cache.addAll(filetocache);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_A1) {
+            console.log("Deleting old cache");
+            return caches.delete(cache);
+          }
+        })
+      );
     })
   );
 });
